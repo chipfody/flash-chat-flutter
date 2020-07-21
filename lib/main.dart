f7b8e58fbb822ddbbf15759048a9f7ee18b434b8
chipfody <60615532+chipfody@users.noreply.github.com>
2020-07-21 14:34:08 -0400

    consolidated widgets

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,6 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
       initialRoute: WelcomeScreen.welcomeId,
       routes: {
         WelcomeScreen.welcomeId: (context) => WelcomeScreen(),
